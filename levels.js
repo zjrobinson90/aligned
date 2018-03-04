a389bc469c8c17ebea1f03fd8a2812633885f49e
Zechariah Robinson <zjrobinson90@gmail.com>
2018-03-04 14:33:28 -0800

    not sure what I've changed

--- levels.js
+++ levels.js
@@ -2,36 +2,17 @@
 
 
 var square = "<div onmouseup='clicked(this)' class='square'></div>";
+var triangle = "<div onmouseup='clicked(this)' class='triangle'></div>";
 
 window.onload = function() {
 	setupGame();
 }
 
 function setupGame(){
-	//creates the shapes
-	var n = 0;
-	for(i=0; i<5; i++){
-		for (j=0; j<2; j++){
-			$(square).appendTo('#parent');
-			$('.square:eq(' + n + ')').attr('id', 'square' + n);
-			n++;
-		}
-	}
-	
-
-	var degree = Math.floor(Math.random() * 11) + 1;
-	var rando = Math.floor(Math.random() * $('.square').length);
-	console.log($('.square').length);
-	$('#square' + rando).css({
-		'-webkit-transform': 'rotate(' + degree + 'deg)',
-		'-moz-transform': 'rotate(' + degree + 'deg)',
-        '-ms-transform': 'rotate(' + degree + 'deg)',
-        '-o-transform': 'rotate(' + degree + 'deg)',
-        'transform': 'rotate(' + degree + 'deg)',
-		});
-		$('#square' + rando).attr('name', 'answer');
+	squarePuzzle_1()
 }
 
+//rotates the selected shape back to 0 or shows a 'wrong' response
 function clicked(x) {
 	var degZero = 0;
 	if($(x).attr('name') == 'answer'){
@@ -49,74 +30,36 @@ function clicked(x) {
 		console.log("WRONG");
 		$(x).css('background-color', 'red');
 	}
-
 }
 
+function squarePuzzle_1(){
+	//makes first square puzzle
+	//creates the shapes and gives them an id
+	var n = 0;
+	for(i=0; i<3; i++){
+		for (j=0; j<2; j++){
+			$(square).appendTo('#parent');
+			//gives the shapes an id by calling their class array position
+			$('.square:eq(' + n + ')').attr('id', 'square' + n);
+			n++;
+		}
+	}
+	//randomly rotates one of the shapes
+	var degree = Math.floor(Math.random() * 11) + 1;
+	var rando = Math.floor(Math.random() * $('.square').length);
+	console.log($('.square').length);
+	$('#square' + rando).css({
+		'-webkit-transform': 'rotate(' + degree + 'deg)',
+		'-moz-transform': 'rotate(' + degree + 'deg)',
+        '-ms-transform': 'rotate(' + degree + 'deg)',
+        '-o-transform': 'rotate(' + degree + 'deg)',
+        'transform': 'rotate(' + degree + 'deg)',
+		});
+		$('#square' + rando).attr('name', 'answer');
+}
 
 
 /*
-var canvas;
-var context;
-var circles=[];
-var squares=[];
-
-function degreesToRadians(degrees) {
-	return(degrees * Math.PI)/180;
-}
-
-function makeShapes(){
-	//creates shadows for all shapes
-	context.shadowColor="black";
-	context.shadowBlur="20";
-	context.shadowOffsetX=5;
-	context.shadowOffsetY=5;
-	
-	for(i = 0; i < 5; i++){
-		circles.push(new circle(100 + (i*200), canvas.height/2));
-	}
-
-	for(i = 0; i < 5; i++){
-		var size = 200;
-		var xPos = 250;
-		var yPos = 250;
-		context.save();
-		if(i == 1){
-			context.translate(size, size);
-			//context.translate(xPos + (size / 2), yPos +  (size / 2));
-			context.rotate(Math.PI / 45);
-			context.translate(-size, -size);
-			//context.translate(-1 * (xPos + (size / 2)), -1 * (yPos +  (size / 2)));
-			squares.push(new square(xPos + (i * (size + 10)), yPos, size));
-		} else {
-			squares.push(new square(xPos + (i * (size + 10)), yPos, size));
-		}
-		context.restore();
-	}
-}
-
-function square(x, y, s){
-	context.beginPath();
-	context.rect(x, y, s, s);
-	context.strokeStyle="black";
-	context.lineWidth=5;
-	context.fillStyle="lightgray";
-	context.stroke();
-	context.fill();
-}
-
-function circle(x, y){
-	context.beginPath();
-	var radius = 100;
-	context.arc(x, y, radius, 0, 2*Math.PI);
-	context.strokeStyle="black";
-	context.lineWidth=5;
-	context.fillStyle="lightgray";
-	context.stroke();
-	context.fill();
-	//context.fill();
-}
-
-
 window.onload = function() {
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
