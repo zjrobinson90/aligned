ea8f5294b2c45bd95cb6fba7fc91947e2020f741
Zechariah Robinson <zjrobinson90@gmail.com>
2018-03-04 15:18:38 -0800

    added refresh on click

--- levels.js
+++ levels.js
@@ -30,6 +30,7 @@ function clicked(x) {
 		console.log("WRONG");
 		$(x).css('background-color', 'red');
 	}
+	setTimeout(window.location.reload.bind(window.location), 750);
 }
 
 function squarePuzzle_1(){
