88ad5f87090c1b666746475d7a719ab562c4c275
Zechariah Robinson <zjrobinson90@gmail.com>
2018-03-09 08:21:01 -0800

    added point system based on time

--- levels.js
+++ levels.js
@@ -1,9 +1,13 @@
-
+// This is my code to run everything and where I'll put the code for different puzzles
 
 var square = "<div onmouseup='clicked(this)' class='square'></div>";
 var triangle = "<div onmouseup='clicked(this)' class='triangle'></div>";
+var totalScore = 0; // sets variable to show the total score of a user (ADD COOKIES TO THIS)
+var startTime; // sets variable to check the start time of the puzzle
+var elapsedTime; // sets variable to check how much time elapses between the start of the puzzle and the first click
+var points; // sets variable to determine how many points are awarded based on time
 
-// Setting up function to vibrate phone
+// sets up function to vibrate phone
 function vibrate() {
 	try{
 		navigator.vibrate([5]);
@@ -12,31 +16,31 @@ function vibrate() {
 
 // make full screen by clicking the DIV "banner"
 function fullScreen() {
-	document.getElementById('banner').addEventListener('click', () => {
-		screenfull.toggle();
-	});
+	$('#banner').click(function(){screenfull.toggle()});
 }
 
-
+// starts everything
 window.onload = function() {
 	setupGame();
 	fullScreen();
-	// checks to see if the screen is full screened (never will be on start) and console logs it
-	console.log("The screen is fullscreen: " + screenfull.isFullscreen);
+	console.log("The screen is full screen: " + screenfull.isFullscreen); // checks to see if the screen is full screened (never will be on start) and console logs it
 }
 
 function setupGame(){
-	// keeps the screen in portrait mode on phones and catches the error thrown when on desktop
+	// keeps the screen in portrait mode on phones and catches the error thrown when on desktop (NOT WORKING)
 	screen.orientation.lock('portrait').catch(function() {// do nothing
 	});
-	// runs the first puzzle
-	squarePuzzle_1();
+	clearPuzzle(); // clears the parent div of anything in it and runs the first puzzle
 }
 
 //rotates the selected shape back to 0 or shows a 'wrong' response
 function clicked(x) {
+	elapsedTime = Date.now() - startTime; // shows how much time has passed since the puzzle started
+	timerPoints(); // sets the points dependent upon the elapsed time
+	console.log(elapsedTime);
 	vibrate(); // vibrates the phone either way
 	var degZero = 0;
+	// checks to see if the shape clicked is the correct one or the wrong one
 	if($(x).attr('name') == 'answer'){
 		console.log("CORRECT");
 		$(x).css({
@@ -48,10 +52,14 @@ function clicked(x) {
 		'transform': 'rotate(' + degZero + 'deg)',
 		});
 		$(x).css('background-color', 'green');
+		totalScore = totalScore + points; // adds 'points' to the total score
+		
 	} else {
 		console.log("WRONG");
 		$(x).css('background-color', 'red');
+		totalScore = totalScore - 5; // subtracts points to the total score
 	}
+	$('#banner').html("Aligned: " + totalScore);
 	// runs the clear puzzle function, starting the next puzzle
 	setTimeout(function(){clearPuzzle();}, 500);
 }
@@ -60,6 +68,21 @@ function clicked(x) {
 function clearPuzzle(){
 	$('#parent').empty();
 	squarePuzzle_1();
+	startTime = Date.now(); // sets the start time timer for how long it takes someone to click
+}
+
+// sets the points based on amount of time elapsed
+function timerPoints(){
+	points = 0;
+	if(elapsedTime <= 500) {
+		points = 15;
+	} else if(elapsedTime >= 501 && elapsedTime <= 1000) {
+		points = 10;
+	} else if(elapsedTime >=1001 && elapsedTime <= 1500) {
+		points = 5;
+	} else {
+		points = 0;
+	}
 }
 
 // Puzzle 1
@@ -88,3 +111,7 @@ function squarePuzzle_1(){
 		});
 		$('#square' + rando).attr('name', 'answer');
 }
+
+
+
+
